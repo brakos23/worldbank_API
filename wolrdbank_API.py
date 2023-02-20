b95a4dc5574324ee28262dcbd3438f50ae4e1cc5
brakos23 <91325166+brakos23@users.noreply.github.com>
2023-02-20 18:35:40 +0100

    Add files via upload

---
+++ wolrdbank_API.py
@@ -0,0 +1,22 @@
+import pymysql
+from flask import Flask, jsonify
+
+conn = pymysql.connect(user='id',
+                       password='Root123*',
+                       database='worldbank',
+                       host='db-world-bank.mysql.database.azure.com',
+                       ssl={'ca': 'C:\\Users\\ray_p\\Downloads\DigiCertGlobalRootCA.crt.pem'})
+
+app = Flask(__name__)
+
+@app.route('/topic', methods=['GET'])
+def get_topic():
+    with conn.cursor() as cursor:
+        sql = "SELECT * FROM topic"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
